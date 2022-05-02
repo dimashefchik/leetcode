3421943b38735e941e936a3049e43fe26571ab39
Dima Shefchik <96975830+dimashefchik@users.noreply.github.com>
2022-05-02 23:00:52 +0300

    Create Two_sum.py

---
+++ Two_sum.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        result = []
+        for x in nums:
+            valid = target - x
+            if valid in nums[nums.index(x)+1:]:
+                result.append(nums.index(x))
+                result.append(nums.index(valid,nums.index(x)+1))
+                break
+
+        return result
